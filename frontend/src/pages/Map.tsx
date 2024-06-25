605967313f18ed959fc9141d3806b9e3c7dbcd1c
s460 <66819532+S460s@users.noreply.github.com>
2024-06-25 16:23:32 +0300

    terry davis is my hero

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,12 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { profile } from "console";
 
 export async function loader() {
-	const res = await fetch("http://localhost:12345/store-layout");
-	const data = await res.json();
-	return { data };
+	const resMap = await fetch("http://localhost:12345/store-layout");
+	const dataMap = await resMap.json();
+	const resPath = await fetch("http://localhost:12345/find-route", {
+		method: "POST",
+		body: JSON.stringify({
+			products: [3, 12, 43, 51, 61, 123, 210, 89, 69, 101, 10, 44],
+		}),
+	});
+	const dataPath = await resPath.json();
+	return { dataMap, dataPath };
 }
 
 export const MyComponent = () => (
@@ -31,8 +37,16 @@ interface DataI {
 	productId: number;
 	checkoutName: string;
 }
+interface PointI {
+	x: number;
+	y: number;
+}
 
-const Grid = ({ gridData }: { gridData: DataI[][] }) => {
+interface PathI {
+	path: PointI[];
+}
+
+const Grid = ({ gridData, path }: { gridData: DataI[][]; path: PathI }) => {
 	const [selectedProductId, setSelectedProductId] = useState(0);
 
 	console.log(selectedProductId);
@@ -40,38 +54,56 @@ const Grid = ({ gridData }: { gridData: DataI[][] }) => {
 		setSelectedProductId(productId);
 	};
 
+	const grid = gridData.map((row, rowIndex) => (
+		<div key={rowIndex} className="flex">
+			{row.map((cell, colIndex) => (
+				<motion.div
+					key={colIndex}
+					className={`w-4 h-4 m-1 shadow-md round-[${Math.floor(
+						Math.random() * 20
+					)}]  ${getColorFromKind(cell.kind, colIndex, rowIndex, path)}`}
+					initial={{ scale: 0 }}
+					animate={{
+						scale: 1,
+					}}
+					transition={{
+						duration: 0.2,
+						delay:
+							rowIndex * 0.04 + colIndex * 0.04 * (cell.kind !== 0 ? 0 : 1),
+					}}
+					onHoverStart={() => {
+						if (cell.kind === 3) handleTap(cell.productId);
+					}}
+				/>
+			))}
+		</div>
+	));
+
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			{gridData.map((row, rowIndex) => (
-				<div key={rowIndex} className="flex">
-					{row.map((cell, colIndex) => (
-						<motion.div
-							key={colIndex}
-							className={`w-4 h-4 m-1 shadow-md round-[${Math.floor(
-								Math.random() * 20
-							)}]  ${getColorFromKind(cell.kind)}`}
-							initial={{ scale: 0 }}
-							animate={{
-								scale: 1,
-							}}
-							transition={{
-								duration: 0.2,
-								delay:
-									rowIndex * 0.04 + colIndex * 0.04 * (cell.kind !== 0 ? 0 : 1),
-							}}
-							onHoverStart={() => {
-								if (cell.kind === 3) handleTap(cell.productId);
-							}}
-						/>
-					))}
-				</div>
-			))}
+			{grid}
 			<h1>{selectedProductId}</h1>
 		</div>
 	);
 };
 
-const getColorFromKind = (kind: number) => {
+const getColorFromKind = (kind: number, x: number, y: number, path: PathI) => {
+	const good = path.path.find((point) => point.x === x && point.y === y);
+	if (good) {
+		if (kind === 0) return "bg-red-300";
+		switch (kind) {
+			case 1:
+				return `bg-gradient-to-r from-blue-500 to-red-300`;
+			case 2:
+				return `bg-gradient-to-r from-bg-green-500 to-red-300`;
+			case 3:
+				return `bg-gradient-to-r from-yellow-700 to-red-300`;
+			case 4:
+				return `bg-gradient-to-r from-purple-500 to-red-300`;
+			default:
+				return "bg-gray-300";
+		}
+	}
 	switch (kind) {
 		case 0:
 			return "dark:bg-white dark:opacity-30 bg-transparent";
@@ -89,13 +121,17 @@ const getColorFromKind = (kind: number) => {
 };
 
 export function Map() {
-	const { data } = useLoaderData() as { data: DataI[][] };
-	console.log(data);
+	const { dataPath, dataMap } = useLoaderData() as {
+		dataMap: DataI[][];
+		dataPath: PathI;
+	};
+
+	console.log(dataPath);
 
 	return (
 		<>
 			<h1>Map</h1>
-			<Grid gridData={data} />
+			<Grid gridData={dataMap} path={dataPath} />
 			<canvas id="map"></canvas>
 		</>
 	);
