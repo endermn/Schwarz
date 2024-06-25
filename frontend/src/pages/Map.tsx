b9eb579ee1aadfd4f59d44cc1f472c601f7f8e27
s460 <66819532+S460s@users.noreply.github.com>
2024-06-25 13:18:47 +0300

    add loader for map

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,3 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+	const res = await fetch("localhost:12345/store-layout");
+	const data = await res.json();
+	return { data };
+}
+
 export function Map() {
+	const { data } = useLoaderData() as any;
+	console.log(data);
 	return <h1>Map</h1>;
 }
