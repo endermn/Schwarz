7516b9d9d45ae6cee97493b8a501e28aa2bfa79b
stomah <stomahinov@gmail.com>
2024-06-26 13:28:52 +0300

    cors

--- backend/cors.go
+++ backend/cors.go
@@ -4,8 +4,9 @@ import "net/http"
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight request
