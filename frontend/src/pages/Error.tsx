e75983b9dc2e692713c84326fd6d28d7e6a3a456
Aleksandar Rangelov <alexbrangelov@gmail.com>
2024-06-26 20:50:53 +0300

    revamp 404 page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -4,10 +4,26 @@ export default function ErrorPage() {
 	const error = useRouteError() as { statusText: string; message: string }; // fix that later
 
 	return (
-		<div id="error-page">
-			<h1>Опа!</h1>
-			<p>Съжаляваме, изникна неочаквана грешка!</p>
-			<p>{error.statusText || error.message}</p>
-		</div>
+		// <div id="error-page">
+		// 	<h1>Опа!</h1>
+		// 	<p>Съжаляваме, изникна неочаквана грешка!</p>
+		// 	<p>{error.statusText || error.message}</p>
+		// </div>
+		<>
+			<main
+				id="error-page"
+				className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8]"
+			>
+				<img
+					src="https://seeklogo.com/images/L/linux-tux-logo-8C1B4FB97E-seeklogo.com.png"
+					alt="Готино пингвинче"
+					className="m-20"
+				/>
+				<h1 className="scroll-m-20 text-6xl font-extrabold">Опа!</h1>
+				<p className="text-xl text-muted-foreground">
+					Нещо се счупи! ({error.statusText || error.message})
+				</p>
+			</main>
+		</>
 	);
 }
